f75d535667be9abe99e94aecdb9469dd31199ea8
Ahmad Hossain <ahmad.awaab@gmail.com>
2022-05-07 14:56:14 -0400

    - Added Edit & Delete icon-buttons - Implemented functionality for Delete - Added 'Add' Button to TodoList Screen

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react'
 import axios from '../axios';
+import { IconButton } from '@mui/material';
+import { Delete, Edit } from '@mui/icons-material';
 
-export default function TodoItem({ todo }) {
-    console.log(`TodoItem: ${JSON.stringify(todo)}`)
+export default function TodoItem({ todo, onDelete, onEdit }) {
     const [checked, setChecked] = useState(todo.isCompleted);
+
     const handleCheck = () => {
         axios.patch('/todo/' + todo._id, { isCompleted: !checked })
             .then(res => console.log(res.data));
 
         setChecked(!checked);
+    }
+    const handleEdit = () => {
+        onEdit(todo)
+    }
+    const handleDelete = () => {
+        onDelete(todo)
     }
 
     return (
@@ -21,7 +29,15 @@ export default function TodoItem({ todo }) {
             </td>
             <td>{todo.title}</td>
             <td>{todo.description}</td>
-            <td>Edit | Delete</td>
+            <td>
+                <IconButton onClick={handleEdit} aria-label="edit" size="small">
+                    <Edit fontSize="small" />
+                </IconButton>
+                |
+                <IconButton onClick={handleDelete} aria-label="delete" size="small">
+                    <Delete fontSize="small" />
+                </IconButton>
+            </td>
         </tr>
     )
 }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,18 +2,38 @@ import React, { useState, useEffect } from "react";
 import axios from '../../axios';
 import TodoItem from "../TodoItem";
 import './TodoList.css';
-
+import { Button } from "@mui/material";
+import { Add } from "@mui/icons-material";
 export default function TodoList() {
     const [todoState, setTodoState] = useState([]);
+    
+    //todo refactor to use this state
+    const [state, setState] = useState({
+        todos: [],
+        isDialogOpen: false,
+    })
 
     useEffect(() => {
         axios.get('/todo')
             .then(res => setTodoState(res.data))
     }, [])
 
+    const deleteTodo = (todo) => {
+        axios.delete('/todo/' + todo._id)
+            .then(res => {
+                console.log(res.data)
+
+                setTodoState(todoState.filter(item => item._id !== todo._id))
+            });
+    }
+    const editTodo = (todo) => {
+        //todo Open dialog for editing Todo
+    }
+
     return (
         <div>
             <h3>Todo List</h3>
+            <Button sx={{ mb: 2 }} variant="contained" startIcon={<Add />}>Add</Button>
             <table className="todos-table">
                 <thead>
                     <tr>
@@ -24,10 +44,13 @@ export default function TodoList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {todoState.map(todo => <TodoItem todo={todo} key={todo._id} />)}
+                    {todoState.map(todo => <TodoItem
+                        todo={todo}
+                        onDelete={deleteTodo}
+                        onEdit={editTodo}
+                        key={todo._id} />)}
                 </tbody>
             </table>
-
         </div>
     )
 }
